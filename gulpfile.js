a8ddbda6299b1b27f5d741cb2152a04c80e592d8
Kris Redding <kredding@autoscribe.com>
2017-07-28 16:23:22 -0400

    Initial Add

---
+++ gulpfile.js
@@ -0,0 +1,113 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var less = require('gulp-less');
+var minifyCSS = require('gulp-minify-css');
+var runSequence = require('run-sequence');
+var del = require('del');
+
+var gulpSequence = require('gulp-sequence');
+var gfi = require("gulp-file-insert");
+var merge = require('merge-stream');
+
+var config = {
+  src: {
+    appJs:[		
+      'app/assets/js/config/*.js',
+      'app/assets/js/components/*.js',
+      'app/assets/js/directives/*.js',
+      'app/assets/js/services/*.js'
+    ],
+    appLess: [
+      'app/assets/less/*.less'
+    ],
+    libsJs: [
+      'app/assets/libs/jquery/dist/jquery.min.js',
+      'app/assets/libs/bootstrap/dist/js/bootstrap.min.js',
+      'app/assets/libs/angular/angular.min.js',
+      'app/assets/libs/angular-sanitize/angular-sanitize.min.js',
+      'app/assets/libs/angular-material/angular-material.min.js',
+      'app/assets/libs/angular-animate/angular-animate.min.js',
+      'app/assets/libs/angular-aria/angular-aria.min.js',
+      'app/assets/libs/angular-messages/angular-messages.min.js',      
+      'app/assets/libs/angular-bootstrap/ui-bootstrap.min.js',
+      'app/assets/libs/angular-bootstrap/ui-bootstrap-tpls.js'
+    ],
+    libsCSS: [
+      'app/assets/libs/angular-material/angular-material.min.css',
+      'app/assets/libs/bootstrap/dist/css/bootstrap.min.css',
+      'app/assets/libs/font-awesome/css/font-awesome.min.css',
+      'app/assets/libs/animate.css/animate.min.css',
+      'app/assets/libs/angular-bootstrap/ui-bootstrap-csp.css'
+    ],
+    libsFonts: [
+      'app/assets/libs/font-awesome/fonts/**',
+      'app/assets/libs/bootstrap/fonts/**'
+    ],
+    htmlFiles: [
+        {'filename': 'index', 'locationname':'home.html'},
+        {'filename': 'test', 'locationname':'test.html'}
+    ],
+    htmlLocation: "app/views/"
+  },
+  dest:{
+    appJs:'public/js',
+    appCSS:'public/css',
+    appFonts:'public/fonts',
+    base: 'public'
+  }
+};
+
+
+gulp.task('clean', function () {
+  return del([config.dest.base+'/*']);
+});
+gulp.task('app-js', function(){
+  // Bundle all JS files into one files
+  return gulp.src(config.src.appJs)
+      .pipe(concat('bundle.js'))
+      .pipe(gulp.dest(config.dest.appJs));
+});
+
+gulp.task('app-less', function(){
+  // Build all Less files into one min CSS
+  return gulp.src(config.src.appLess)
+      .pipe(concat('styles.min.css'))
+      .pipe(less())
+      .pipe(minifyCSS())
+      .pipe(gulp.dest(config.dest.appCSS));
+});
+
+gulp.task('lib-js', function(){
+  // Bundle all JS Library files into one files
+  return gulp.src(config.src.libsJs)
+      .pipe(concat('libs.min.js'))
+      .pipe(gulp.dest(config.dest.appJs));
+});
+gulp.task('lib-css', function(){
+  // Bundle all JS Library files into one files
+  return gulp.src(config.src.libsCSS)
+      .pipe(concat('libs.min.css'))
+      .pipe(gulp.dest(config.dest.appCSS));
+});
+
+gulp.task('lib-fonts', function(){
+  // Move all fonts files into one the public fonts folder
+  return gulp.src(config.src.libsFonts)
+      .pipe(gulp.dest(config.dest.appFonts));
+});
+
+gulp.task('build-html', function(){
+    //var fileMap = config.src.htmlFiles;
+    
+    var tasks = config.src.htmlFiles.map(function(fileObj){
+      console.log("Building File: " + fileObj.filename);
+      return gulp.src(config.src.htmlLocation + 'shared/_layout.html')
+                .pipe(gfi({"/* INSERT BODY */": config.src.htmlLocation + fileObj.locationname  }))
+                .pipe(rename(function(path){ path.basename = fileObj.filename; }))
+                .pipe(gulp.dest(config.dest.base));      
+    });
+    
+    return merge(tasks);
+});
+gulp.task('all', gulpSequence('clean', ['lib-fonts', 'lib-css', 'lib-js','app-js', 'app-less'], 'build-html'));
