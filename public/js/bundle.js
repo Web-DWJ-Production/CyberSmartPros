26da698189859a6e82e003307f56a964ff8821e9
Alex Goley <alexgoley@Alexs-MBP.fios-router.home>
2017-08-17 20:31:20 -0400

    home remove period

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -51,14 +51,14 @@ pageControllers.controller('HomePage', [function(){
       ];
 
       ctrl.latestNews = [
-          {'date':new Date("2015-08-01 00:00:00"), 'text':'Our founder started exploring his business idea for Cyber Smart Pros.'},
-          {'date':new Date("2015-11-26 00:00:00"), 'text':'Cyber Smart Pros has officially become a Virginia Limited Liability Company (LLC).'},
-          {'date':new Date("2016-06-01 00:00:00"), 'text':'Cyber Smart Pros gets it first customer.'},          
-          {'date':new Date("2017-05-01 00:00:00"), 'text':'Cyber Smart Pros becomes a verified Service Disabled Veteran Owned Small Business (SDVOSB).'},
-          {'date':new Date("2017-06-01 00:00:00"), 'text':'Cyber Smart Pros becomes a certified SWaM and veteran-owned business in the State of Virginia.'},        
-          {'date':new Date("2017-08-01 00:00:00"), 'text':'Cyber Smart Pros hires it first employee.'},
-          {'date':new Date("2017-08-18 00:00:00"), 'text':'We have partnered with DWJ Production to deploy a new website focused at capturing the company ideals and display our ever growing web presence.'},           
-          {'date':new Date("2017-09-01 00:00:00"), 'text':'Cyber Smart Pros opens an office in Leesburg, VA.'}
+          {'date':new Date("2015-08-01 00:00:00"), 'text':'Our founder started exploring his business idea for Cyber Smart Pros'},
+          {'date':new Date("2015-11-26 00:00:00"), 'text':'Cyber Smart Pros has officially become a Virginia Limited Liability Company (LLC)'},
+          {'date':new Date("2016-06-01 00:00:00"), 'text':'Cyber Smart Pros gets it first customer'},          
+          {'date':new Date("2017-05-01 00:00:00"), 'text':'Cyber Smart Pros becomes a verified Service Disabled Veteran Owned Small Business (SDVOSB)'},
+          {'date':new Date("2017-06-01 00:00:00"), 'text':'Cyber Smart Pros becomes a certified SWaM and veteran-owned business in the State of Virginia'},        
+          {'date':new Date("2017-08-01 00:00:00"), 'text':'Cyber Smart Pros hires it first employee'},
+          {'date':new Date("2017-08-18 00:00:00"), 'text':'We have partnered with DWJ Production to deploy a new website focused at capturing the company ideals and display our ever growing web presence'},           
+          {'date':new Date("2017-09-01 00:00:00"), 'text':'Cyber Smart Pros opens an office in Leesburg, VA'}
       ];
       ctrl.dividerImg = 'images/cityview-above.jpg';
 
